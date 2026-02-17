0abe123b518ad8c72ec0d312336db5c96dc8fbca
Andzzio <andzzio924@gmail.com>
2026-02-16 21:32:59 -0500

    Add whatsapp floating button functionality

--- lib/presentation/widgets/whatsapp_floating_button.dart
+++ lib/presentation/widgets/whatsapp_floating_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:virtual_catalog_app/presentation/providers/business_provider.dart';
 
 class WhatsappFloatingButton extends StatelessWidget {
@@ -8,11 +9,20 @@ class WhatsappFloatingButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final businessProvider = context.read<BusinessProvider>();
+    final phone = businessProvider.business?.whatsappNumber ?? "";
     return FloatingActionButton(
-      onPressed: () {
-        final phone =
-            context.read<BusinessProvider>().business?.whatsappNumber ?? "";
-        debugPrint(phone);
+      onPressed: () async {
+        if (phone.isEmpty) return;
+        final String message = "Hola, me gustaría ver su catálogo";
+        final url = Uri.parse(
+          "https://wa.me/$phone?text=${Uri.encodeComponent(message)}",
+        );
+        if (await canLaunchUrl(url)) {
+          await launchUrl(url, mode: LaunchMode.externalApplication);
+        } else {
+          debugPrint("No se puedo enviar el mensaje a whatsapp");
+        }
       },
       shape: CircleBorder(),
       backgroundColor: Colors.greenAccent,
